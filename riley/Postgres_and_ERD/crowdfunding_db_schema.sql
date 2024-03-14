4b92435f6cca33ba5eeef5f78de81b0a31d715f1
TaylorMater <rt.taylor.riley@gmail.com>
2024-03-14 07:38:01 -0500

    added QDB code, ERD png, and schema from QDB

---
+++ riley/Postgres_and_ERD/crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(48)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" MONEY   NOT NULL,
+    "pledged" MONEY   NOT NULL,
+    "outcome" BOOLEAN   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" CHAR(2)   NOT NULL,
+    "currency" CHAR(3)   NOT NULL,
+    "launched_date" TIMESTAMP   NOT NULL,
+    "end_date" TIMESTAMP   NOT NULL,
+    "category_id" CHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" CHAR(4)   NOT NULL,
+    "category" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(24)   NOT NULL,
+    "last_name" VARCHAR(24)   NOT NULL,
+    "email" TEXT   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcateogry" (
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    "subcategory" VARCHAR(24)   NOT NULL,
+    CONSTRAINT "pk_subcateogry" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcateogry" ("subcategory_id");
+
